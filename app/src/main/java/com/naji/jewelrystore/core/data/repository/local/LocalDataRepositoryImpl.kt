21d6f28e1ec599b32c5ad160c314aac3f2af19ad
NajiAbed31 <nageaped31@gmail.com>
2024-12-21 12:18:34 +0200

    Preparing LocalDataRepository

--- app/src/main/java/com/naji/jewelrystore/core/data/repository/local/LocalDataRepositoryImpl.kt
+++ app/src/main/java/com/naji/jewelrystore/core/data/repository/local/LocalDataRepositoryImpl.kt
@@ -1,4 +1,21 @@
 package com.naji.jewelrystore.core.data.repository.local
 
-class LocalDataRepositoryImpl {
+import android.content.Context
+import com.naji.jewelrystore.core.domain.repository.LocalDateRepository
+
+class LocalDataRepositoryImpl : LocalDateRepository {
+    private val prefsName = "prefs"
+    private val userIdKey = "userId"
+
+    override fun saveUserId(context: Context, userId: String) {
+        context.getSharedPreferences(prefsName, Context.MODE_PRIVATE)
+            .edit()
+            .putString(userIdKey, userId)
+            .apply()
+    }
+
+    override fun getUserId(context: Context): String {
+        return context.getSharedPreferences(prefsName, Context.MODE_PRIVATE)
+            .getString(userIdKey, null) ?: ""
+    }
 }
--- app/src/main/java/com/naji/jewelrystore/core/domain/repository/LocalDateRepository.kt
+++ app/src/main/java/com/naji/jewelrystore/core/domain/repository/LocalDateRepository.kt
@@ -1,4 +1,9 @@
 package com.naji.jewelrystore.core.domain.repository
 
+import android.content.Context
+
 interface LocalDateRepository {
+    fun saveUserId(context: Context, userId: String)
+
+    fun getUserId(context: Context): String
 }
